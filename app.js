1e6ff32893bb10cd5d8ee6d2938a9013f1a5a91f
shubyte <76104945+shubyte@users.noreply.github.com>
2020-12-16 15:58:47 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const ip = document.getElementById("ipAddress");
+const loc = document.getElementById("location");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
+const search = document.getElementById("search");
+let endpoint= "http://ip-api.com/json/?fields=33612753" //583665";
+let mymap = L.map("mapid").setView([51.505, -0.09], 1);
+
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoibGFzaGEzMTAiLCJhIjoiY2tmdHp1ZzRlMHBpMjJ5czhvZ21jeGdnaiJ9.iUlg-65qjkpL9P07zDlgpA",
+  }
+).addTo(mymap);
+
+ const getIp = () => {
+   fetch(endpoint)
+     .then((res) => {
+       return res.json();
+     })
+     .then((response) => {
+       mymap.flyTo([response.lat, response.lon], 13);
+       L.marker([response.lat, response.lon]).addTo(mymap);
+       ip.innerText = `${response.query}`;
+       loc.innerText = `${response.city}, ${response.country}`;
+       timezone.innerText = `UTC ${response.offset / 3600}:00`;//${response.timezone}
+       isp.innerText = `${response.isp}`;
+       console.log(response);
+     });
+ }
+   getIp();
+  
+  
+search.addEventListener("click", () => {
+  let querr = document.getElementById("IP").value;
+  endpoint = `http://ip-api.com/json/${querr}?fields=33612753`;
+  let expression = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
+  if (expression.test(querr)) {
+    // good IP
+   getIp()
+  } else {
+    // bad IP
+    alert("Please write valid IP");
+  }
+});
